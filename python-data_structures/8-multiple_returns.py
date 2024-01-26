3917542f0ac22144f2ce646c928e301e169171ef
lunarinaa <aydan.akbarova@gmail.com>
2024-01-26 03:49:25 -0800

    return adjusted

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    tuple = ()
     if sentence[0] == 0:
         return None
     else:
         return len(sentence), sentence[0]
+    return tuple
