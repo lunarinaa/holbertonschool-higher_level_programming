48abedbb0d6eb4711114b138b71e6d6e61c251a4
lunarinaa <aydan.akbarova@gmail.com>
2024-02-06 07:32:29 -0800

    print usin #

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Defining aRectangle Class"""
+
+
+class Rectangle:
+    """Empty class representing a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing a new rectangle.
+
+        Args:
+            width: the width of the rectangle
+            height: the height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """determine the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """determines the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates area of the rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """Calculates perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        """Prints rectangle with # character"""
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+        rectangle = []
+        for i in range(self.__height):
+            [rectangle.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rectangle.append("\n")
+        return "".join(rectangle)
