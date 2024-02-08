baa4a8d8e7b9a3c0dff88f451200d88dd42bca9f
lunarinaa <aydan.akbarova@gmail.com>
2024-02-08 12:12:48 -0800

    load from json

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function"""
+
+import json 
+
+
+def load_from_json_file(filename):
+    """Creates object from a JSON file"""
+    with open(filename) as f:
+        return json.load(f)
