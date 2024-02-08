5bb7429e71cc647699d638140ff946c72690a322
lunarinaa <aydan.akbarova@gmail.com>
2024-02-08 12:03:01 -0800

    save to json

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines function"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes object to a text file"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
