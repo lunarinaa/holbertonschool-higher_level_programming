c317154169c80d440e67d404e13b0cdbd305e54e
lunarinaa <aydan.akbarova@gmail.com>
2024-02-08 23:17:46 -0800

    replace attributes

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a new class"""
+
+
+class Student:
+    """new classs"""
+
+    def __init__(self, first_name, last_name, age):
+        """Function than initializes a student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of Sudent"""
+        if (type(attrs) is list and
+                all(type(elem) is str for elem in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all the attributes of the Student"""
+        for i, j in json.items():
+            setattr(self, i, j)
