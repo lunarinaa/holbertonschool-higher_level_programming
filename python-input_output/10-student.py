24725d28c291b5e3c0fe456e6194ef6a3c73f3ce
lunarinaa <aydan.akbarova@gmail.com>
2024-02-08 23:09:54 -0800

    to json with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a new class"""
+
+
+class Student:
+    """new classs"""
+
+    def __init__(self, first_name, last_name, age):
+        """Function than initializes a student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of Sudent"""
+        if (type(attrs) == list and
+            all(type(elem) == str for elem in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
