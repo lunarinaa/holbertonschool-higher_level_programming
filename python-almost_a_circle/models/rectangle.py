db8bac6f2f1c74b5f1c7c59fde94bf4ce50ff295
lunarinaa <aydan.akbarova@gmail.com>
2024-02-14 04:32:57 -0800

    updated

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,7 +99,7 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
-            print(" " * self.__y + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Returns user friendly representation of the rectangle class"""
