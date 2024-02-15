6fa1de1467a38b96c03bdb6c7436fe633e6fa068
Aydan <aydan.akbarova@gmail.com>
2024-02-15 13:00:08 +0400

    pep validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -117,7 +117,7 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             if key in attributes:
                 setattr(self, key, value)
-    
+
     def to_dictionary(self):
         """ Returns the dictionary representation of the Rectangle"""
         return {'id': self.id,
