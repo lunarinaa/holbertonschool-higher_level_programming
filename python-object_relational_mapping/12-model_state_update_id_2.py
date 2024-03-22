eca51d2d593547d90e996b753ce8a2126ebc46d7
lunarinaa <aydan.akbarova@gmail.com>
2024-03-23 02:24:35 +0400

    filter by state id

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id == 2):
+    for state in session.query(State).order_by(State.id).filter(State.id == 2):
         state.name = "New Mexico"
     session.commit()
     session.close()
