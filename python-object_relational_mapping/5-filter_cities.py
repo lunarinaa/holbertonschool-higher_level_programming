21b0a06526bf426ef574a134ed94fc581d3e3d7c
lunarinaa <aydan.akbarova@gmail.com>
2024-03-22 19:11:26 +0400

    checking

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""connecting databases"""
+""" connecting database """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    db_connection = MySQLdb.connect(host="localhost", user=argv[1],
-                                    passwd=argv[2], db=argv[3], port=3306)
-    cursor = db_connection.cursor()
-    cursor.execute(
-                    "SELECT cities.name FROM cities \
-                    JOIN states ON cities.state_id = states.id \
-                    WHERE states.name = %s \
-                    ORDER BY cities.id",
-                    (argv[4],)
-                   )
-    output = cursor.fetchall()
-    cities_for_state = [city[0] for city in output]
-    print(", ".join(cities_for_state))
-    cursor.close()
+    db = MySQLdb.connect(
+      host="localhost",
+      user=sys.argv[1],
+      passwd=sys.argv[2],
+      database=sys.argv[3],
+      port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities RIGHT JOIN states ON cities.state_id =\
+        states.id WHERE states.name = '{}'".format(sys.argv[4]))
+    results = cur.fetchall()
+    print(", ".join([result[2] for result in results]))
+    cur.close()
