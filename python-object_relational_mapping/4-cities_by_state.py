a9276e322c31b6071f92f0868be96e89fa6828d5
lunarinaa <aydan.akbarova@gmail.com>
2024-03-22 15:58:55 +0400

    inner join

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                                    passwd=argv[2], db=argv[3], port=3306)
     cursor = db_connector.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities"
-                   "JOIN states ON cities.state_id = states.id"
+                   "INNER JOIN states ON cities.state_id = states.id"
                    "ORDER BY cities.id")
     output = cursor.fetchall()
     for i in output:
