36ad8a64a7734a64d5320d0cdc71a7ae27d44fcc
lunarinaa <aydan.akbarova@gmail.com>
2024-03-22 18:39:28 +0400

    Revert "cheking"
    This reverts commit 1daf971d2d7a14cdda625afc03a9356c44989702.

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-import sys
+"""connecting databases"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    db_connection = MySQLdb.connect(host="localhost", user=argv[1],
+                                    passwd=argv[2], db=argv[3], port=3306)
+    cursor = db_connection.cursor()
+    cursor.execute(
+                    "SELECT cities.name FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id",
+                    (argv[4],)
+                   )
+    output = cursor.fetchall()
+    cities_for_state = [city[0] for city in output]
+    print(", ".join(cities_for_state))
+    cursor.close()
