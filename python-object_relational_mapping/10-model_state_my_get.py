54f4981ddd0cbd2b985ba2c03aa01c57cf81fded
lunarinaa <aydan.akbarova@gmail.com>
2024-03-23 00:02:43 +0400

    state name parametrized

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -7,14 +7,13 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.crreate_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    met = 0
-    for state in session.query(State).order_by(State.id):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            met = 1
-    if met == 0:
+    state_name = sys.argv[4]
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print("{}".format(state.id))
+    else:
         print("Not found")
     session.close()
