d8d4243692a2516dd48c018a8aba69241cc1ea63
lunarinaa <aydan.akbarova@gmail.com>
2024-03-22 16:03:45 +0400

    module

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -3,7 +3,7 @@
 
 
 import MySQLdb
-import sys.argv
+from sys import argv
 if __name__ == "__main__":
     db_connector = MySQLdb.connect(host="localhost", user=argv[1],
                                    passwd=argv[2], db=argv[3], port=3306)
