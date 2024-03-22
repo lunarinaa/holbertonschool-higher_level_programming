72a1ccdcb8d4f21e57c573336d24089f139aa0d0
lunarinaa <aydan.akbarova@gmail.com>
2024-03-22 21:08:29 +0400

    check

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" database connector"""
-
+"""Lists states"""
 
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
-    db_connector = MySQLdb.connect(host="localhost", user=argv[1],
-                                   passwd=argv[2], db=argv[3], port=3306)
-    cursor = db_connector.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-                   INNER JOIN states ON cities.state_id = states.id \
-                   ORDER BY cities.id")
-    output = cursor.fetchall()
-    for i in output:
-        print(i)
-    cursor.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    conn.close()
