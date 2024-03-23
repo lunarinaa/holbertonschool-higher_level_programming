f114f665b4a60859e3b63c64b78a789b28d66956
lunarinaa <aydan.akbarova@gmail.com>
2024-03-23 18:53:35 +0400

    table name attribute

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 class City(Base):
     """class definition"""
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
