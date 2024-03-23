fcceab0094d0362704c335f3d72096155afd842e
lunarinaa <aydan.akbarova@gmail.com>
2024-03-23 18:32:02 +0400

    typo corrected

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,7 @@ from model_statw import Base, State
 from sys import argv
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
